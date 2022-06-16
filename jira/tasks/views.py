9f8300a2a10507d045284cd997e1637ae2cca5d8
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-06-16 16:20:17 +0500

    created models and list tasks view

--- jira/tasks/views.py
+++ jira/tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Task
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('hi')
+
+
+def list_todos(request):
+    tasks = Task.objects.all()
+    res = '<ul>'
+    for t in tasks:
+        res += '<li>{}</li>'.format(t)
+    res += '</ul>'
+    return HttpResponse(res)
